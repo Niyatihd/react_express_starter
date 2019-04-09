38fe56fbf0b29a7134db51a6a4de056398b54aad
Niyati Desai <niyatihd@gmail.com>
2019-04-09 16:51:05 -0700

    Connected to mongoDB

--- routes/api/items.js
+++ routes/api/items.js
@@ -10,8 +10,19 @@ const Item = require('../../models/Item');
 //if this was in ServiceWorkerRegistration.js File, app.get('/api/items')
 router.get('/', (req, res) => { 
   Item.find()
-  .sort({ date: -1 })
-  .then(items => res.json(items));
+    .sort({ date: -1 })
+    .then(items => res.json(items));
+}); 
+
+// @route POST api/items
+// @description Create a Post
+// @access Public (coz we do not auth incorporated yet, so everything is public)
+router.post('/', (req, res) => { 
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json(item));
 }); 
 
 module.exports = router;
